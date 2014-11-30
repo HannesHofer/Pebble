a9ec0cb627821bc0f8f6c75b5503c85341145d21
Hannes Hofer <Hannes.Hofer@gmail.com>
2014-11-30 23:30:13 +0100

    add messaging & finish sunrise/set calc code
    add messaging and finish sunrise/ set calculation code
    add bugfixes to suncalc code

--- src/utils.c
+++ src/utils.c
@@ -1,17 +1,24 @@
 #include "utils.h"
+#include "my_math.h"
+
+#define ZENITH_OFFICIAL 90.83333
+#define ZENITH_CIVIL 96.0
+#define ZENITH_NAUTICAL 102.0
+#define ZENITH_ASTRONOMICAL 108.0
+
+#define ZENITH ZENITH_CIVIL
+
 // calculate sunset & sunrise 
 // http://williams.best.vwh.net/sunrise_sunset_algorithm.htm
-#define PI 3.141592653589793
 
-float my_atan(float x)
+float adjust360(float value)
 {
-  if (x > 0)
-    return (M_PI / 2)*(0.596227 * x + x * x)/(1 + 2 * 0.596227 * x + x * x);
-  else
-    return -(my_atan(-x));
+  while (value < 0)
+    value += 360.0;
+  while (value > 360.0)
+    value -= 360;
+  return value;
 }
-
-
 
 int calcday(int year, int month, int day)
 {
@@ -26,32 +33,61 @@ float convertlonghour(float longitude, int sunset, int year, int month, int day)
 {
   // convert the longitude to hour value and calculate an approximate time
   float lngHour = longitude / 15;
-  int diff = 6
+  int diff = 6;
   if (sunset)
     diff = 18;
     
   return calcday(year, month, day) + ((diff - lngHour) / 24); 
 }
 
-float adjust360(float value)
+float calcsun(float longitude, float latitude, int sunset, int year, int month, int day)
 {
-  while (value < 0)
-    value += 360.0;
-  while (L > 360.0)
-    value -= 360;
-  return value;
-}
-
-float calcsun(float longitude, int sunset, int year, int month, int day)
-{
+  float t = convertlonghour(longitude, sunset, year, month, day);
   // sun anomaly
-  float M = 0.9856 * t) - 3.289;
+  float M = (0.9856 * t) - 3.289;
   
-  // sun ture longitude
-  float L = M + (1.916 * sin_lookup(M)) + (0.020 * sin_lookup(2 * M)) + 282.634;
+  // sun true longitude
+  float L = M + (1.916 * my_sin(M)) + (0.020 * my_sin(2 * M)) + 282.634;
   L = adjust360(L);
   
-  floar RA = 
+  // sun right ascension
+  float RA = my_atan(0.91764 * my_tan(L));
+  RA = adjust360(RA);
   
+  // right ascension value needs to be in the same quadrant as L
+  float Lquadrant = (int)(L / 90) * 90;
+  float RAquadrant = (int)(RA / 90) * 90;
+  RA += Lquadrant + RAquadrant;
+  RA /= 15;
   
+  // sun declination
+  float sinDec = 0.39782 * my_sin(L);
+  float cosDec = my_cos(my_asin(sinDec));
+  
+  // sun local hour angle
+  float cosH = (my_cos(ZENITH) - (sinDec * my_sin(latitude))) / (cosDec * my_cos(latitude));
+  if (cosH > 1 || cosH < -1)
+    return -1.0;
+  
+  // convert into hours
+  float H;
+  if (sunset)
+    H = (180.0f / M_PI) * my_acos(cosH);
+  else
+    H = 360 - ((180.0f / M_PI) * my_cos(cosH));
+  
+  H /= 15;
+  
+  // calculate local mean time
+  float T = H + RA - (0.06571 * t) - 6.622;
+  
+  // convert to UTC
+  float UT = T - (longitude / 15);
+  
+  if (UT < 0)
+    UT += 24;
+  else if (UT > 24)
+    UT -= 24;
+  
+  return UT;
 }
