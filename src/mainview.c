84238c0e2389fda3274c8e7ad8358f68162dc856
Hannes Hofer <Hannes.Hofer@gmail.com>
2014-11-30 16:21:27 +0100

    changes to mainview
    move some functions from mainview to utils

--- src/mainview.c
+++ src/mainview.c
@@ -1,5 +1,5 @@
-#include <pebble.h>
- 
+#include "utils.h"
+
 // BEGIN AUTO-GENERATED UI CODE; DO NOT MODIFY
 static Window *s_window;
 static GBitmap *s_res_rect_background_white;
@@ -123,24 +123,11 @@ static void destroy_ui(void) {
 }
 // END AUTO-GENERATED UI CODE
 
-// custom globals
-static char* days[] = {"Sonntag", "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag"};
-static uint8_t bat_level = 0;
-static GBitmap *s_res_nobluetooth;
-
 static void setDate(struct tm *current_time) {
   static char DateBuffer[] = "00/00/0000"; 
   text_layer_set_text(Day, days[current_time->tm_wday]);
   strftime(DateBuffer, sizeof("00/00/0000"), "%d/%m/%Y", current_time);
   text_layer_set_text(actDate, DateBuffer);
-}
-
-static GColor drawcolor = GColorBlack;
-static GColor not_drawcolor() {
-  if (drawcolor == GColorBlack)
-       return GColorWhite;
-  else
-       return GColorBlack;
 }
 
 void handle_tick(struct tm *tick_time, TimeUnits units_changed)
@@ -173,20 +160,6 @@ void handle_seconds(struct tm *tick_time, TimeUnits units_changed)
     handle_tick(tick_time, MINUTE_UNIT);
 
 }
-
-static uint8_t seconds_startdraw[] = {66, 75, 84, 93, 102, 111, 121, 130, 140, 1,
-                                      11, 22, 33, 44, 55, 66, 77, 88, 99, 110,
-                                      121, 132, 142, 140, 130, 121, 111, 102, 93, 84, 
-                                      75, 66, 57, 48, 39, 30, 20, 11, 0, 142,
-                                      132, 121, 110, 99, 88, 77, 66, 55, 44, 33,
-                                      22, 11, 1, 0, 11, 20, 30, 39, 48, 57};
-
-// {{-1, 8}, {2, 8}, {7, 3}, {4, 0}, {-1, 5}, {-1, 8}}
-static GPath *edge_path[4];
-static const GPathInfo BOLT_PATH_INFO = {
-  .num_points = 6,
-  .points = (GPoint []) {{-1, 8}, {2, 8}, {7, 3}, {4, 0}, {-1, 5}, {-1, 8}}
-};
 
 void draw_edge(GContext* ctx, uint8_t edgenr, GColor mydrawcolor) {
   graphics_context_set_fill_color(ctx, mydrawcolor);
